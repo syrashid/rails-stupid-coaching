a46857668ce15f378d70efe4a8b893a2367ab868
Sy Rashid <sy.rashid.11@gmail.com>
2020-04-25 10:33:15 +1000

    updated nokogiri

---
+++ test/system/questions_test.rb
@@ -0,0 +1,34 @@
+require "application_system_test_case"
+
+class QuestionsTest < ApplicationSystemTestCase
+  test "visiting /ask renders the form" do
+    visit ask_url
+    assert_selector "p", text: "Ask your coach anything"
+  end
+
+  test "saying Hello yields a grumpy response from the coach" do
+    visit ask_url
+    fill_in "question", with: "Hello"
+    click_on "Ask"
+
+    assert_text "I don't care, get dressed and go to work!"
+  end
+
+  test "saying I'm going to work yields a positive response from the coach" do
+    visit ask_url
+    fill_in "question", with: "I am going to work"
+    click_on "Ask"
+
+    assert_text "Great!"
+  end
+
+  test "Asking a question yields yelling from the coach" do
+    visit ask_url
+    fill_in "question", with: "What should I do today?"
+    click_on "Ask"
+
+    assert_text "Silly question, get dressed and go to work!"
+    take_screenshot
+  end
+
+end
